8bbf99c899bdf016cfa2061428887ee72190b393
Tahir khan <tahirwpdeveloper3@gmail.com>
2024-03-24 17:07:19 +0500

    Ready to publish

--- app.js
+++ app.js
@@ -3,8 +3,4 @@ var tableLirstNum = +prompt("Enter Your Table End Number");
 
 for(var i = 1; i<=tableLirstNum; i++){
     document.write(`${tableFirstNum} x ${i} = ${tableFirstNum*i} <br>`)
-}if(tableFirstNum==null){
-    alert("Please enter Table Number");
-}else if(tableLirstNum==null){
-    document.write("Enter Your Ending Point");
 }
